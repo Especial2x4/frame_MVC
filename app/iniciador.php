3135c20c47753c17e45bb27b81971405d26f2a8a
Especial2x4 <especial2x4@gmail.com>
2024-05-19 15:55:41 -0300

    Se obtiene la url del navegador mediante el Core

--- app/iniciador.php
+++ app/iniciador.php
@@ -1,6 +1,11 @@
 
 <?php
 
-echo "HOLA MUNDO";
+// Carga librerias
+require_once "librerias/Base.php";
+require_once "librerias/Controlador.php";
+require_once "librerias/Core.php";
+
+echo "HOLA MUNDO desde el INICIADOR";
 
 ?>
