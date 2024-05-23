6e4f75a8ae30362f0c560a08a3dbe20bd20ba21b
Especial2x4 <especial2x4@gmail.com>
2024-05-22 23:26:47 -0300

    actualización del método getUrl() para trozar la url ingresada en el navegador

--- app/iniciador.php
+++ app/iniciador.php
@@ -6,6 +6,6 @@ require_once "librerias/Base.php";
 require_once "librerias/Controlador.php";
 require_once "librerias/Core.php";
 
-echo "HOLA MUNDO desde el INICIADOR";
+//echo "HOLA MUNDO desde el INICIADOR";
 
 ?>
