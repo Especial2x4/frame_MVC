6656dfab585b585f6f73cce30b97ca9cb2511ef6
Especial2x4 <especial2x4@gmail.com>
2024-06-05 19:01:49 -0300

    trabajando en los estilos (las redirecciones están bastante mal)

--- app/vistas/paginas/inicio.php
+++ app/vistas/paginas/inicio.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="app\public\css\estilos.css">
+    <link rel="stylesheet" href="../app/public/css/estilos.css">
 </head>
 <body>
     <h1>Prueba de carga desde la vista Inicio</h1>
